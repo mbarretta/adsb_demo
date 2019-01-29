caa45d4ce9e082fc64059cfb2b9e7de53c817c2f
Mike Barretta <mike.barretta@gmail.com>
2019-01-29 16:12:03 -0500

    removed lambda stuff

--- src/main/groovy/com/barretta/elastic/adsb/ADSBCollector.groovy
+++ src/main/groovy/com/barretta/elastic/adsb/ADSBCollector.groovy
@@ -1,31 +1,23 @@
 package com.barretta.elastic.adsb
 
-import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.elastic.barretta.clients.ESClient
 import groovy.util.logging.Slf4j
 
 @Slf4j
-class ADSBCollector implements RequestHandler<String, String> {
-    private def properties
+class ADSBCollector {
+    private static def properties
 
-    String handleRequest(String request, Context context) {
-        loadProperties()
-        doIt()
-    }
+    static void main(String[] args) {
+        properties = new ConfigSlurper().parse(GroovyClassLoader.getSystemResource("properties.groovy"))
 
-    def doIt() {
-        loadProperties()
         log.info("fetching all states")
         def allStates = OpenSkyNetworkClient.getAllStates()
         log.info(" ...found [${allStates.states.size()}]")
 
         def client = new ESClient(properties.esClient as ESClient.Config)
-        client.bulk([(ESClient.BulkOps.INSERT): allStates.states.collect { it.properties.remove("class"); it.properties }])
+        client.bulk([(ESClient.BulkOps.INSERT): allStates.states.collect {
+            it.properties.remove("class"); it.properties
+        }])
         log.info("done")
-    }
-
-    private def loadProperties() {
-        properties = new ConfigSlurper().parse(GroovyClassLoader.getSystemResource("properties.groovy"))
     }
 }
