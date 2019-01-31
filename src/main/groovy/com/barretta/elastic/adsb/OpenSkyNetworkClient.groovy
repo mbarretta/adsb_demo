0a95df9822f7a45ac6f113cfab47ac217d557866
Mike Barretta <mike.barretta@gmail.com>
2019-01-30 21:02:39 -0500

    ...fixed a lil testing change I'd put in there

--- src/main/groovy/com/barretta/elastic/adsb/OpenSkyNetworkClient.groovy
+++ src/main/groovy/com/barretta/elastic/adsb/OpenSkyNetworkClient.groovy
@@ -95,7 +95,7 @@ class OpenSkyNetworkClient {
 
         //this try{} is a bit ugly, but simple means of dealing with empty results, which end are sent as a 404
         try {
-            def rawResponse = new JsonSlurper().parse("$URL/flights/all?begin=${time - 2000}&end=$time".toURL())
+            def rawResponse = new JsonSlurper().parse("$URL/flights/all?begin=${time - 20}&end=$time".toURL())
             rawResponse?.each { flight ->
                 try {
                     def flightObj = new Flight()
