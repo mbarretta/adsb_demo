41ec1492c01c22f587787ba7c37c28855bb8ba1e
Mike Barretta <mike.barretta@gmail.com>
2019-02-19 12:40:30 -0500

    ...

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -8,7 +8,7 @@ appender("FILE", RollingFileAppender) {
         totalSizeCap = "1GB"
     }
     encoder(PatternLayoutEncoder) {
-        pattern = "%date{HH:mm:ss.SSS} [%thread] %-5level %logger{35} - %msg%n"
+        pattern = "%date{MM.dd.yyyy HH:mm:ss.SSS} [%thread] %-5level %logger{35} - %msg%n"
     }
 }
 appender("STDOUT", ConsoleAppender) {
